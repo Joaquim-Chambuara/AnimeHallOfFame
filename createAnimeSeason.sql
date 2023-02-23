6a2eee6552dd4bd7a769ef45311dd3ccdd5729d4
Joaquim-Chambuara <122440707+Joaquim-Chambuara@users.noreply.github.com>
2023-02-23 09:40:07 +0200

    Update createAnimeSeason.sql

--- createAnimeSeason.sql
+++ createAnimeSeason.sql
@@ -7,7 +7,7 @@ GO
 CREATE TABLE anime_season
 (
   anime_series_id int IDENTITY(1,1) NOT NULL,
-  anime_season_numberr int NOT NULL,
+  anime_season_number int NOT NULL,
   anime_season_episodes int NOT NULL,
   anime_season_episode_duration time(0) NOT NULL,
   anime_season_rating decimal(3,1) NOT NULL,
